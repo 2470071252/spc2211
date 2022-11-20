538674b2ea26ff15b68d6f0a8fed518517399a0d
jason <jason@gmail.com>
2022-11-20 14:19:58 +0800

    添加了 ApplicationContext 测试案例

--- spring-ioc/src/main/java/cn/tedu/spring/SpringIocApplication.java
+++ spring-ioc/src/main/java/cn/tedu/spring/SpringIocApplication.java
@@ -1,13 +1,29 @@
 package cn.tedu.spring;
 
+import cn.tedu.spring.bean.DemoBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SpringIocApplication {
 
+    static Logger logger = LoggerFactory.getLogger(SpringIocApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(SpringIocApplication.class, args);
+        //run 返回当前Spring 的容器对象
+        ApplicationContext context =
+            SpringApplication.run(SpringIocApplication.class, args);
+        //获取类型唯一的 Java Bean 对象
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        logger.debug("{}", demoBean);
+        //按照ID获取Bean
+        List<String> list = context.getBean("names", List.class);
+        logger.debug("list {}", list);
     }
 
 }
---
+++ spring-ioc/src/test/java/cn/tedu/spring/ContextTests.java
@@ -0,0 +1,31 @@
+package cn.tedu.spring;
+
+import cn.tedu.spring.bean.DemoBean;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+@SpringBootTest
+public class ContextTests {
+
+    Logger logger = LoggerFactory.getLogger(ContextTests.class);
+
+    /**
+     * 利于@Autowired方式可以获得当前的 ApplicationContext
+     */
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    void tests(){
+        DemoBean demoBean = context.getBean(DemoBean.class);
+        List<String> list = context.getBean("names", List.class);
+        logger.debug("{} {}", demoBean, list);
+    }
+
+}
