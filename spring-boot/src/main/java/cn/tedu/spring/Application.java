05d9ed8ceca873378482dc8cee1a61f6a5e2a8ee
RobinLiu <liucangsong@mgail.com>
2022-11-27 16:35:43 +0800

    测试 替换 Tomcat 为Jetty

--- spring-boot/src/main/java/cn/tedu/spring/Application.java
+++ spring-boot/src/main/java/cn/tedu/spring/Application.java
@@ -1,9 +1,16 @@
 package cn.tedu.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * exclude：排除
+ * 利用 exclude 属性，排除自动类型，显示取消自动配置功能
+ */
+//@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class )
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
