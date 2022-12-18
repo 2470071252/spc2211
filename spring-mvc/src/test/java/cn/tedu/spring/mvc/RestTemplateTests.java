2cf3d681c78d5eb87b8e9c00cecf2df06bd12d1d
RobinLiu <liucangsong@mgail.com>
2022-12-18 15:42:14 +0800

    添加了 Rest 客户端测试

--- spring-mvc/src/test/java/cn/tedu/spring/mvc/RestTemplateTests.java
+++ spring-mvc/src/test/java/cn/tedu/spring/mvc/RestTemplateTests.java
@@ -7,22 +7,36 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 
 @SpringBootTest
 public class RestTemplateTests {
 
     Logger logger = LoggerFactory.getLogger(RestTemplateTests.class);
 
+    RestTemplate restTemplate = new RestTemplate();
     @Test
     void getForObject(){
         /*
          * 测试 使用 RestTemplate 作为客户端，访问Rest服务器
          */
-        RestTemplate restTemplate = new RestTemplate();
         //  如果使用URL参数，需要设置参数占位符{0} {1} {2} ...
         String url = "http://localhost:8080/users/{0}";
         //  调用时候，进行参数替换， 按照顺序进行替换， User.class表示返回类型
         User user = restTemplate.getForObject(url, User.class, 4);
         logger.debug("返回： {}", user);
+    }
+
+    @Test
+    void users(){
+        /*
+         * 获取多个用户信息
+         */
+        String url = "http://localhost:8080/users";
+        User[] users = restTemplate.getForObject(url, User[].class);
+        for(User user: users){
+            logger.debug("{}", user);
+        }
     }
 }
