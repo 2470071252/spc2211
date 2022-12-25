2f6f12310e60af426d3042a773fddd9030dfc9c1
RobinLiu <liucangsong@mgail.com>
2022-12-25 15:33:16 +0800

    添加了 检查，事务的代理

--- spring-jdbc-tx/src/test/java/cn/tedu/spring/service/UserServiceTests.java
+++ spring-jdbc-tx/src/test/java/cn/tedu/spring/service/UserServiceTests.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @SpringBootTest
 public class UserServiceTests {
@@ -15,6 +16,22 @@ public class UserServiceTests {
 
     @Test
     void userService(){
+        //在业务层中添加 @Transactional 后userService
+        //的类型，就会出现 动态代理类。这个动态代理类提供
+        //事务管理功能。
         logger.debug(" Class {}", userService.getClass().getName());
+    }
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    @Test
+    void txMgr(){
+        // transactionManager 事务管理器，@Transactional 底层
+        // 就是利用 transactionManager 实现功能，transactionManager
+        // 可以有不同平台的实现，支持 Hibernate Spring-JDBC MyBatis
+        // transactionManager 自动支持，统一管理，简单配置后，就能够
+        // 统一处理事务。
+        logger.debug("{}", transactionManager.getClass().getName());
     }
 }
